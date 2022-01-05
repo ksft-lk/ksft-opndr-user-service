aed5f7e42952690c10ebe64d49ad3af297193316
ksfonseka <kasun.sampath@kochasoft.com>
2022-01-05 23:38:06 +0530

    TBD-77 added firestore data lib

--- src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
+++ src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
@@ -6,29 +6,25 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
 
 
-@Component
+@Configuration
 public class FirebaseConfig {
 
 	
-	
-	public FirebaseConfig(@Value("${firebase-app-key}")String firebaseAppKey) {
-		System.out.println("firebaseAppKey - "+firebaseAppKey);
-		 try { 
+	@Bean
+	public Firestore getFireStore(@Value("${firebase-app-key}")String firebaseAppKey) throws IOException {
 			 InputStream serviceAccount = new ByteArrayInputStream(firebaseAppKey.getBytes()); 
-			 FirebaseOptions.Builder builder = FirebaseOptions.builder(); 
-			 FirebaseOptions build = builder.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+			 GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+			 FirestoreOptions build = FirestoreOptions.newBuilder().setCredentials(credentials)
 			 .build(); 
-			 FirebaseApp.initializeApp(build); 
-		 } catch (IOException e) {
-			 e.printStackTrace(); 
-		 }
+			return build.getService(); 
 		 
 
 	}
--- src/main/java/com/kochasoft/opendoor/userservice/UserserviceApplication.java
+++ src/main/java/com/kochasoft/opendoor/userservice/UserserviceApplication.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.ComponentScan;
 @OpenAPIDefinition
 @SpringBootApplication
 @ComponentScan("com.kochasoft.opendoor.userservice")
-@EnableReactiveFirestoreRepositories
 public class UserserviceApplication {
 	
 	public static void main(String[] args) {
