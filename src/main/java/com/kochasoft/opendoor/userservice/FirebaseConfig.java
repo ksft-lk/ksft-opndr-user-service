a220a4426afc7d6874bddbaa89fc8847f729d966
ksfonseka <kasun.sampath@kochasoft.com>
2022-01-05 22:11:14 +0530

    TBD-77 Removed firebase config

--- src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
+++ src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
@@ -1,26 +1,26 @@
-package com.kochasoft.opendoor.userservice;
-
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-
-@Component
-public class FirebaseConfig {
-
-    @Value("${firebase-app-key}")
-    String firebaseAppKey;
-	
-	public FirebaseConfig() {
-		/*
-		 * try { InputStream serviceAccount = new
-		 * ByteArrayInputStream(firebaseAppKey.getBytes()); FirebaseOptions.Builder
-		 * builder = FirebaseOptions.builder(); GoogleCredentialsProvider
-		 * FirebaseOptions build =
-		 * builder.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-		 * .build(); FirebaseApp.initializeApp(build); } catch (IOException e) {
-		 * e.printStackTrace(); }
-		 */
-
-	}
-}
+//package com.kochasoft.opendoor.userservice;
+//
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//
+//@Component
+//public class FirebaseConfig {
+//
+//    @Value("${firebase-app-key}")
+//    String firebaseAppKey;
+//	
+//	public FirebaseConfig() {
+//		/*
+//		 * try { InputStream serviceAccount = new
+//		 * ByteArrayInputStream(firebaseAppKey.getBytes()); FirebaseOptions.Builder
+//		 * builder = FirebaseOptions.builder(); GoogleCredentialsProvider
+//		 * FirebaseOptions build =
+//		 * builder.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//		 * .build(); FirebaseApp.initializeApp(build); } catch (IOException e) {
+//		 * e.printStackTrace(); }
+//		 */
+//
+//	}
+//}
