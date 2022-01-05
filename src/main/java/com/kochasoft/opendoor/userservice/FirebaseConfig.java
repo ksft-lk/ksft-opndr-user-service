4e130e179104c107e033c971eb2956b9a59dbd61
ksfonseka <kasun.sampath@kochasoft.com>
2022-01-05 21:46:05 +0530

    TBD-77 change to access secret key

--- src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
+++ src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
@@ -1,16 +1,9 @@
 package com.kochasoft.opendoor.userservice;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.google.api.gax.core.GoogleCredentialsProvider;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 
 @Component
 public class FirebaseConfig {
--- src/main/java/com/kochasoft/opendoor/userservice/controller/UserController.java
+++ src/main/java/com/kochasoft/opendoor/userservice/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.kochasoft.opendoor.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.api.client.util.Value;
 
 @RestController
 @RequestMapping("/user/v1")
