e34ccb5d9683ef607b9d2c53f05c95443e13fd70
ksfonseka <kasun.sampath@kochasoft.com>
2022-01-05 21:36:16 +0530

    TBD-77 print the key

--- src/main/java/com/kochasoft/opendoor/userservice/controller/UserController.java
+++ src/main/java/com/kochasoft/opendoor/userservice/controller/UserController.java
@@ -1,14 +1,34 @@
 package com.kochasoft.opendoor.userservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
+
+import com.google.api.client.util.Value;
 
 @RestController
+@RequestMapping("/user/v1")
 public class UserController {
 
-    @Value("${firebase-app-key}")
+//	@Autowired
+//	UserService userService;
+//	
+	@Value("${firebase-app-key}")
     String firebaseAppKey;
+	
+	private static final String SUCCESS = "SUCCESS";
+	private static final String FAILED = "FAILED";
+	
+//	@PostMapping("/registerUser")
+//	String registerUser(User user) {
+//		try {
+//			userService.createUser(user);
+//			return SUCCESS;
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			return FAILED;
+//		}
+//	}
 
     @GetMapping("/")
     String _getUser(){
