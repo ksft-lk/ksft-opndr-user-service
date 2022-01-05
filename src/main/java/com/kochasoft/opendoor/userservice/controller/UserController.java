39377469747fc33df50de55ae3df81bfebc1a1d3
ksfonseka <kasun.sampath@kochasoft.com>
2022-01-05 13:07:21 +0530

    added secret manager dependency

--- src/main/java/com/kochasoft/opendoor/userservice/controller/UserController.java
+++ src/main/java/com/kochasoft/opendoor/userservice/controller/UserController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Value(${"firebase-app-key"})
+    String fireBaseKey;
+
     @GetMapping("/")
     String _getUser(){
-        return "Hello World";
+        return "Hello World "+fireBaseKey;
     }
 }
