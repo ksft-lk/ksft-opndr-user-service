75d2f078627c0088c2515524b619fac47aa2f5f6
ksfonseka <kasun.sampath@kochasoft.com>
2022-01-05 22:31:38 +0530

    TBD-77 added firebase config

--- src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
+++ src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
@@ -1,26 +1,37 @@
-//package com.kochasoft.opendoor.userservice;
-//
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Component;
-//
-//
-//@Component
-//public class FirebaseConfig {
-//
-//    @Value("${firebase-app-key}")
-//    String firebaseAppKey;
-//	
-//	public FirebaseConfig() {
-//		/*
-//		 * try { InputStream serviceAccount = new
-//		 * ByteArrayInputStream(firebaseAppKey.getBytes()); FirebaseOptions.Builder
-//		 * builder = FirebaseOptions.builder(); GoogleCredentialsProvider
-//		 * FirebaseOptions build =
-//		 * builder.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//		 * .build(); FirebaseApp.initializeApp(build); } catch (IOException e) {
-//		 * e.printStackTrace(); }
-//		 */
-//
-//	}
-//}
+package com.kochasoft.opendoor.userservice;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.google.api.gax.core.GoogleCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+
+@Component
+public class FirebaseConfig {
+
+    @Value("${firebase-app-key}")
+    String firebaseAppKey;
+	
+	public FirebaseConfig() {
+		
+		 try { 
+			 InputStream serviceAccount = new ByteArrayInputStream(firebaseAppKey.getBytes()); 
+			 FirebaseOptions.Builder builder = FirebaseOptions.builder(); 
+			 FirebaseOptions build = builder.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+			 .build(); 
+			 FirebaseApp.initializeApp(build); 
+		 } catch (IOException e) {
+			 e.printStackTrace(); 
+		 }
+		 
+
+	}
+}
