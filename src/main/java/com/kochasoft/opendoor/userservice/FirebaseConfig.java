2ad547bda980a102f8b75c699e9fc43e1aad6df7
ksfonseka <kasun.sampath@kochasoft.com>
2022-01-05 20:27:31 +0530

    TBD-77 firebaseconfig commented

--- src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
+++ src/main/java/com/kochasoft/opendoor/userservice/FirebaseConfig.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.google.api.gax.core.GoogleCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -18,17 +19,15 @@ public class FirebaseConfig {
     String firebaseAppKey;
 	
 	public FirebaseConfig() {
-
-		try {
-			InputStream serviceAccount = new ByteArrayInputStream(firebaseAppKey.getBytes());	
-			FirebaseOptions.Builder builder = FirebaseOptions.builder();
-			
-			FirebaseOptions build = builder.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-		    .build();
-			FirebaseApp.initializeApp(build);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		/*
+		 * try { InputStream serviceAccount = new
+		 * ByteArrayInputStream(firebaseAppKey.getBytes()); FirebaseOptions.Builder
+		 * builder = FirebaseOptions.builder(); GoogleCredentialsProvider
+		 * FirebaseOptions build =
+		 * builder.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+		 * .build(); FirebaseApp.initializeApp(build); } catch (IOException e) {
+		 * e.printStackTrace(); }
+		 */
 
 	}
 }
