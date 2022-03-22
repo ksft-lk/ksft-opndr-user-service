250a5999bf00f197e2ae07bdde3edd9c5d4601a0
Sudeera Sandaruwan Jayasinghe <sudeera.sandaruwan@kochasoft.com>
2022-03-22 10:14:51 +0530

    Moved devices to a root level collection

---
+++ src/main/java/com/kochasoft/opendoor/userservice/domain/Device.java
@@ -0,0 +1,48 @@
+package com.kochasoft.opendoor.userservice.domain;
+
+import com.google.cloud.firestore.annotation.DocumentId;
+import lombok.ToString;
+import org.springframework.cloud.gcp.data.firestore.Document;
+
+@Document(collectionName = "devices")
+@ToString
+public class Device {
+
+  @DocumentId private String id;
+
+  private String userId;
+  private String token;
+  private Status status;
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+}
