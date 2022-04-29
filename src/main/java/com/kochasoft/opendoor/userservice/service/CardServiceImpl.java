4431af5107cdc771fc34b02887ec213471b75030
ksfonseka <kasun.sampath@kochasoft.com>
2022-04-29 15:59:58 +0530

    default card creation

--- src/main/java/com/kochasoft/opendoor/userservice/service/CardServiceImpl.java
+++ src/main/java/com/kochasoft/opendoor/userservice/service/CardServiceImpl.java
@@ -6,6 +6,7 @@ import com.kochasoft.opendoor.userservice.dto.ResponseDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +23,7 @@ public class CardServiceImpl implements CardService {
 
         final HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", token);
+        headers.setContentType(MediaType.APPLICATION_JSON);
   
         final HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
   
